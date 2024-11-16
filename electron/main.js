5bc974808ed308273e9b8b55809d9968f9562ebb
ChangYu Huang <jack.mainwork@gmail.com>
2024-11-16 13:31:47 -0500

    First commit.

---
+++ electron/main.js
@@ -0,0 +1,20 @@
+import { app, BrowserWindow } from "electron";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+let mainWindow;
+
+app.whenReady().then(() => {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: __dirname + '/preload.js',
+    },
+  });
+
+  mainWindow.loadURL("http://localhost:5173");
+});
