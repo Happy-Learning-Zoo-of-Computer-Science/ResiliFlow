c2e9692a7cbb46b85c64040370a75ac318e70f78
ChangYu Huang <jack.mainwork@gmail.com>
2024-12-10 23:26:56 -0500

    Buttons will change style when selected.

--- src/renderer/pages/SelectConfigurations.tsx
+++ src/renderer/pages/SelectConfigurations.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react'
+import { Button } from 'antd'
 
 interface SelectConfigurationsProps {
   selectedLanguage: string
@@ -15,47 +16,59 @@ const SelectConfigurations: React.FC<SelectConfigurationsProps> = ({
   selectedLanguage,
   setSelectedConfigurations,
 }) => {
-  const [configurations, setConfigurations] = useState<string[]>([])
 
+  // Variant of each button.
+  const [variants, setVariants] = useState<{ [key: string]: 'outlined' | 'solid' }>({});
+
+  // Get supported configuration files for the selected language.
+  const [configurations, setConfigurations] = useState<{ [key: string]: any }>({});
   useEffect(() => {
-    const loadConfigurations = async () => {
-      setConfigurations(await fetchConfigurations(selectedLanguage))
-    }
-    loadConfigurations()
-  }, [selectedLanguage])
+    fetchConfigurations(selectedLanguage).then((configurations) => {
+      setConfigurations(configurations);
+      setVariants(Object.keys(configurations).reduce((acc, key) => {
+        acc[key] = 'outlined';
+        return acc;
+      }, {} as { [key: string]: 'outlined' | 'solid' }));
+    });
+  }, [selectedLanguage]);
 
+  // Handle function to update the selected configurations. 
   const handle = (configuration: string) => {
     setSelectedConfigurations((previousSelected) => {
       if (previousSelected.includes(configuration)) {
+        setVariants({ ...variants, [configuration]: 'outlined' });
         return previousSelected.filter((selected) => selected !== configuration)
       } else {
+        setVariants({ ...variants, [configuration]: 'solid' });
         return [...previousSelected, configuration]
       }
     })
-  }
+  };
 
+  // Return a list of checkboxes for each configuration file and text bars for required data.
   return (
     <div>
       <h2>Select configuration files to be generated.</h2>
-      {configurations.map((configuration) => (
-        <button key={configuration} onClick={() => handle(configuration)}>
+      {Object.keys(configurations).map((configuration: string) => (
+        <Button key={configuration} id={configuration} color='default' variant={variants[configuration]} onClick={() => handle(configuration)}>
           {configuration}
-        </button>
+        </Button>
       ))}
     </div>
-  )
+  );
+
 }
 
 /**
  * Get available configuration files for the language.
  * @param selectedLanguage A supported programming language.
- * @returns A list of available configuration files.
+ * @returns A dictionary of available configuration files and their required data.
  */
 const fetchConfigurations = async (
   selectedLanguage: string,
-): Promise<string[]> => {
+): Promise<Object> => {
   if (selectedLanguage === 'None') {
-    return [];
+    return {} as Object;
   }
 
   try {
@@ -66,13 +79,12 @@ const fetchConfigurations = async (
       throw new Error(`Failed to fetch configurations: ${response.statusText}`);
     }
 
-    const dictionary: Record<string, unknown> = await response.json();
-    const configurations = Object.keys(dictionary);
-    console.log("Get " + configurations);
+    const configurations: Object = await response.json();
     return configurations;
+
   } catch (error) {
     console.error('Error fetching configurations:', error);
-    return []; // Return an empty list on error.
+    return {} as Object;
   }
 };
 
