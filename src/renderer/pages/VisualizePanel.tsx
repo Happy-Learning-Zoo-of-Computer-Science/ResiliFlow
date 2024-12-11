cd4c228a955a85351a031c963e4de785252e287c
zjkung1123 <zjkung1123@gmail.com>
2024-12-10 23:03:12 -0500

    Update VisualizePanel styles and enhance node interaction with info display

--- src/renderer/pages/VisualizePanel.tsx
+++ src/renderer/pages/VisualizePanel.tsx
@@ -6,24 +6,73 @@ import {
   useNodesState,
   type Node,
   type Edge,
+  MarkerType,
 } from '@xyflow/react'
 
 import '@xyflow/react/dist/style.css'
-import { Button, Dropdown, MenuProps, Space } from 'antd'
+import { Button, Card, Dropdown, MenuProps, Space } from 'antd'
 import { DownOutlined } from '@ant-design/icons'
 import '../assets/css/VisualizePanel/VisualizePanel.css'
-
+interface NodeInfo {
+  title: string
+  content: string
+}
 export const VisualizePanel: React.FC = () => {
-  const initialNodes: Node[] = [
-    { id: '1', position: { x: 300, y: 100 }, data: { label: 'Build' } },
-    { id: '2', position: { x: 300, y: 200 }, data: { label: 'Test' } },
+  const [selectedNodeInfo, setSelectedNodeInfo] =
+    React.useState<NodeInfo | null>(null)
+  const nodeInfoMap = [
+    ['Install', 'pip install -r requirements.txt'],
+    ['Test', 'pytest'],
+    ['Lint', "pylint $(git ls-files '*.py')"],
   ]
-  const initialEdges: Edge[] = [{ id: 'e1-2', source: '1', target: '2' }]
+  const initialNodes: Node[] = [
+    {
+      id: '1',
+      position: { x: 300, y: 100 },
+      data: { label: 'Install' },
+      style: { backgroundColor: 'red' },
+    },
+    {
+      id: '2',
+      position: { x: 300, y: 200 },
+      data: { label: 'Test' },
+      style: { backgroundColor: 'blue' },
+    },
+  ]
+  const initialEdges: Edge[] = [
+    {
+      id: 'e1-2',
+      source: '1',
+      target: '2',
+      // animated: true,
+      markerEnd: {
+        type: MarkerType.ArrowClosed,
+        width: 20,
+        height: 20,
+        color: '#FF0072',
+      },
+    },
+  ]
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes)
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges)
 
   const onConnect = useCallback(
-    (params: any) => setEdges((eds) => addEdge(params, eds)),
+    (params: any) =>
+      setEdges((eds) =>
+        addEdge(
+          {
+            ...params,
+            // animated: true,
+            markerEnd: {
+              type: MarkerType.ArrowClosed,
+              width: 20,
+              height: 20,
+              color: '#FF0072',
+            },
+          },
+          eds,
+        ),
+      ),
     [setEdges],
   )
 
@@ -38,14 +87,20 @@ export const VisualizePanel: React.FC = () => {
           y: nds[nds.length - 1].position.y + 50,
         },
         data: { label },
+        style:
+          label === 'Install'
+            ? { backgroundColor: 'red' }
+            : label === 'Test'
+            ? { backgroundColor: 'blue' }
+            : { backgroundColor: 'green' },
       },
     ])
   }
 
   const items: MenuProps['items'] = [
     {
-      key: 'Build',
-      label: 'Build',
+      key: 'Install',
+      label: 'Install',
     },
     {
       key: 'Test',
@@ -63,6 +118,16 @@ export const VisualizePanel: React.FC = () => {
   const validNodes = () => {
     console.log(nodes)
   }
+  function showNodeInfo(event: React.MouseEvent, node: Node): void {
+    console.log(node)
+    const nodeInfo = nodeInfoMap.find(
+      (element) => element[0] === node.data.label,
+    )
+    if (nodeInfo) {
+      setSelectedNodeInfo({ title: nodeInfo[0], content: nodeInfo[1] })
+    }
+  }
+
   return (
     <div className="mainFrame">
       <Dropdown menu={menuProps}>
@@ -82,7 +147,13 @@ export const VisualizePanel: React.FC = () => {
         onNodesChange={onNodesChange}
         onEdgesChange={onEdgesChange}
         onConnect={onConnect}
+        onNodeClick={showNodeInfo}
       />
+      {selectedNodeInfo && (
+        <Card title={selectedNodeInfo.title} style={{ width: 300 }}>
+          <p>{selectedNodeInfo.content}</p>
+        </Card>
+      )}
     </div>
   )
 }
